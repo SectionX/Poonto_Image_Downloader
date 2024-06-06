a9fd7215a7cb3d0b6a92d6a909c6a3fdd38864ee
Eli Stouraitis <h.stoyraitis@gmail.com>
2024-06-06 15:17:31 +0300

    Clean up dir

--- server.py
+++
@@ -1,9 +0,0 @@
-"""
-Starts the test server with the reload parameter.
-It's hardcoded to listen on port 8080.
-"""
-
-import uvicorn
-
-if __name__ == '__main__':
-    uvicorn.run('Image_Downloader.app:app', port=8080, reload=True)
